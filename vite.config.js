0142c654afeffb2f9a807492d09d6ad27d4f8c76
Md Shariful Islam <devshariful99@gmail.com>
2025-06-02 15:47:11 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/frontend.css',
+                'resources/css/dashboard.css',
                 'resources/js/app.js'
             ],
             refresh: true,
