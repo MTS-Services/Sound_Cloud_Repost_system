47858184a8199f2f38f557e919748b9e8f8d5cba
Md Shariful Islam <devshariful99@gmail.com>
2025-06-03 09:19:09 +0600

    Updated

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -5,9 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RepostChain - Under Construction</title>
-    <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
+
+    @vite(['resources/css/app.css'])
     <style>
         body {
             background-color: #121212;
