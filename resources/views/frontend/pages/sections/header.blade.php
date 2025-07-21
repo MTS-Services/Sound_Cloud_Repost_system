4b65c92e90aac57c1dd883038abac8212fbbffb3
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-07-21 16:17:20 +0600

    update

--- resources/views/frontend/pages/sections/header.blade.php
+++ resources/views/frontend/pages/sections/header.blade.php
@@ -28,7 +28,7 @@
                     <i class="fas fa-moon hidden dark:block"></i>
                 </button>
 
-                @auth('web')
+                @auth
                     <a href="{{ route('dashboard') }}"
                         class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
                         <svg class="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
