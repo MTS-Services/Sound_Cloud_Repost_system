53bf5903d771ffa0a383a9d9cea296822cb0c621
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-07-22 17:57:43 +0600

    Repost feed on working

--- app/Http/Controllers/Backend/User/RepostFeedController.php
+++ app/Http/Controllers/Backend/User/RepostFeedController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Backend\User;
 
 use App\Http\Controllers\Controller;
+use App\Services\Admin\TrackService;
 use Illuminate\Http\Request;
 
 class RepostFeedController extends Controller
 {
+    protected TrackService $trackService;
+
+    public function __construct(TrackService $trackService)
+    {
+        $this->trackService = $trackService;
+    }
     public function repostFeed(){
-        return view('backend.user.repost-feed');
+        $data['tracks'] = $this->trackService->getTracks()->get();
+        return view('backend.user.repost-feed', $data);
     }
 }
