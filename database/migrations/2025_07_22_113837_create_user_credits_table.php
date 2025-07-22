6e23e9345634299cb294f9f9403a2a533f4bb5d7
Shahidul Islam <engjewel57@gmail.com>
2025-07-22 17:58:51 +0600

    update

--- database/migrations/2025_07_22_113837_create_user_credits_table.php
+++ database/migrations/2025_07_22_113837_create_user_credits_table.php
@@ -14,27 +14,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_followers', function (Blueprint $table) {
+        Schema::create('user_credits', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('sort_order')->default(0);
             $table->string('user_urn')->index();
-            $table->unsignedBigInteger('avatar_urn')->unique();
-            $table->unsignedBigInteger('soundcloud_id')->unique();
-            $table->string('soundcloud_urn')->unique();
-            $table->string('soundcloud_kind');
-            $table->string('permalink_url')->unique();
-            $table->string('uri')->unique();
-            $table->string('username')->uniqde();
-            $table->string(' permalink')->unique();
-            $table->string('first_name')->nullable();
-            $table->string('last_name')->nullable();
-            $table->string('full_name')->nullable();
+            $table->decimal( 10, 2)->default(0);
+
+
             $table->timestamps();
             $table->softDeletes();
             $this->addAdminAuditColumns($table);
 
             $table->foreign('user_urn')->references('urn')->on('users')->onDelete('cascade')->onUpdate('cascade');
-            $table->unique(['user_urn', 'soundcloud_id']);
+            $table->unique(['user_urn']);
         });
     }
 
@@ -43,6 +35,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_followers');
+        Schema::dropIfExists('user_credits');
     }
 };
