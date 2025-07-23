f5e05d34e231e32a03a0321a3c286d028334a7c5
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-07-23 08:56:25 +0600

    some chagne

--- resources/views/backend/user/layouts/partials/header.blade.php
+++ resources/views/backend/user/layouts/partials/header.blade.php
@@ -1,29 +1,26 @@
 <header class="bg-bg-light-primary dark:bg-bg-dark-primary shadow-lg px-6 py-4 sticky top-0 z-30 w-full">
     <div class="flex justify-between items-center">
-        <!-- Search Bar Centered -->
-        <form class="flex-1 flex justify-center">
-            <div class="relative w-full max-w-md">
+        <!-- Search Bar Added -->
+        <form class="flex-1 flex justify-center ">
+            <div class="relative w-full max-w-md items-center">
                 <span class="absolute inset-y-0 left-0 flex items-center pl-4">
                     <i data-lucide="search" class="w-5 h-5 text-text-light-muted dark:text-text-dark-muted"></i>
                 </span>
                 <input type="search" placeholder="Search..."
                     class="w-full pl-12 pr-4 py-3 rounded-lg bg-bg-light-primary dark:bg-bg-dark-tertiary border-2 border-bg-light-muted dark:border-bg-dark-muted
-                   focus:border-[#EF4444]! dark:focus:border-[#EF4444]!
-                   text-text-light-primary dark:text-text-dark-secondary
-                   placeholder:text-text-light-muted dark:placeholder:text-text-dark-muted shadow-sm outline-none transition" />
+                    focus:border-[#F54A00]! dark:focus:border-[#F54A00]!
+                    text-text-light-primary dark:text-text-dark-secondary
+                    placeholder:text-text-light-muted dark:placeholder:text-text-dark-muted shadow-sm outline-none transition" />
             </div>
         </form>
-
-
-
+        
         <div class="flex items-center space-x-4 ml-8">
             <div class="text-sm text-text-light-primary dark:text-text-dark-secondary">
                 <span class="font-semibold">75</span> {{ __('Credits') }}
             </div>
             <div class="relative">
                 <i data-lucide="bell" class="w-6 h-6 text-text-light-primary dark:text-text-dark-muted"></i>
-                <span
-                    class="absolute -top-1 -right-1 bg-[#EF4444] text-white text-xs rounded-full w-4 h-4 flex items-center justify-center">1</span>
+                <span class="absolute -top-2 -right-1 bg-red-500  text-white text-xs rounded-full w-4 h-4 flex items-center justify-center">1</span>
             </div>
             <!-- Theme Toggle Button (Only Light/Dark) -->
             <button @click="$store.theme.toggleTheme()"
@@ -51,7 +48,7 @@
                     <li>
                         <div
                             class="flex items-center space-x-2 border-b border-bg-light-muted dark:border-bg-dark-muted px-6 py-4 w-full">
-                            <img src="{{ auth()->user()->avatar }}" alt="{{ auth()->user()->name }}"
+                            <img src="{{ auth()->user()->avatar }}" alt="{{ auth()->user()->name }} "
                                 class="w-8 h-8 rounded-full">
                             <div class="text-sm">
                                 <span
