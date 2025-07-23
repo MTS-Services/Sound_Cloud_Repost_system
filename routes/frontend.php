1d4a3a3660fbd17c8bbbb385254c64e0fdfe3bac
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-07-23 16:26:38 +0600

    paypal done

--- routes/frontend.php
+++ routes/frontend.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\PaypalController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['as' => 'f.'], function () {
@@ -8,3 +9,6 @@ Route::group(['as' => 'f.'], function () {
     Route::get('/', [HomeController::class, 'landing'])->name('landing');
 
 });
+Route::get('/products/payment', [PaypalController::class, 'paypalPaymentLink'])->name('paypal.paymentLink');
+Route::get('/products/payment/success', [PaypalController::class, 'paypalPaymentSuccess'])->name('paypal.paymentSuccess');
+Route::get('/products/payment/cancel', [PaypalController::class, 'paypalPaymentCancel'])->name('paypal.paymentCancel');
