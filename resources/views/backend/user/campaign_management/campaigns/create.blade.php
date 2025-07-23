d119cbcf3e06cc880148eb4c478e234bbe375bfa
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-07-23 12:18:16 +0600

    change

--- resources/views/backend/user/campaign_management/campaigns/create.blade.php
+++ resources/views/backend/user/campaign_management/campaigns/create.blade.php
@@ -7,7 +7,7 @@
         <div class="glass-card rounded-2xl p-6 mb-6">
             <div class="flex items-center justify-between">
                 <h2 class="text-xl font-bold text-text-black dark:text-text-white">{{ __('Create Campaign') }}</h2>
-                <x-button href="{{ route('cm.campaigns.index') }}" icon="undo-2" type='info' permission="campaign-list">
+                <x-button href="{{ route('user.cm.campaigns.index') }}" icon="undo-2" type='info' permission="campaign-list">
                     {{ __('Back') }}
                 </x-button>
             </div>
