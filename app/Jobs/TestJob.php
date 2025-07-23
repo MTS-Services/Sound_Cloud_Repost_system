ef5c669382bd6e47f7be861f5adf1d4638eb4162
Md Shariful Islam <devshariful99@gmail.com>
2025-07-23 15:58:36 +0600

    Update TestJob.php

--- app/Jobs/TestJob.php
+++ app/Jobs/TestJob.php
@@ -2,25 +2,22 @@
 
 namespace App\Jobs;
 
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
 class TestJob implements ShouldQueue
 {
-    use Queueable;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * Create a new job instance.
-     */
     public function __construct()
     {
         //
     }
 
-    /**
-     * Execute the job.
-     */
     public function handle(): void
     {
         Log::info('✅ TestJob executed successfully at ' . now());
