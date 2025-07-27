4e774b48872cd3aa5d82f9d7e1a725353a8c26f9
Md Shariful Islam <devshariful99@gmail.com>
2025-07-27 13:11:07 +0600

    Updated

--- resources/views/backend/user/campaign_management/campaigns/campaign.blade.php
+++ resources/views/backend/user/campaign_management/campaigns/campaign.blade.php
@@ -78,7 +78,7 @@
 
                             <!-- Track Details -->
                             <div class="flex-1 flex flex-col justify-between p-2">
-                                <x-sound-cloud.sound-cloud-player :track="$campaign->music" height="166" visual="false" />
+                                <x-sound-cloud.sound-cloud-player :track="$campaign->music" visual="false" />
                             </div>
                         </div>
                     </div>
