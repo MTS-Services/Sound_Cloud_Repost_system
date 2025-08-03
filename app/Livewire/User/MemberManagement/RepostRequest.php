9a9c50e49750e843944b4f8ad97b4f9c609f73cd
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-08-03 17:46:19 +0600

    Update RepostRequest.php

--- app/Livewire/User/MemberManagement/RepostRequest.php
+++ app/Livewire/User/MemberManagement/RepostRequest.php
@@ -255,7 +255,6 @@ class RepostRequest extends Component
 
             // Repost the track to SoundCloud
             $response = $httpClient->post("{$this->baseUrl}/reposts/tracks/{$request->track_urn}");
-dd($response);
             if ($response->successful()) {
                 // If SoundCloud returns a repost ID, capture it
                 $soundcloudRepostId = $response->json('id');
