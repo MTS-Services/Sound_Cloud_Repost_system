09f0fe23dc2ea38e9be678f35924643e04541384
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-08-03 17:38:21 +0600

    Update RepostRequest.php

--- app/Livewire/User/MemberManagement/RepostRequest.php
+++ app/Livewire/User/MemberManagement/RepostRequest.php
@@ -254,7 +254,7 @@ class RepostRequest extends Component
             ]);
 
             // Repost the track to SoundCloud
-            $response = $httpClient->post("{$this->baseUrl}/reposts/tracks/{$request->track->urn}");
+            $response = $httpClient->post("{$this->baseUrl}/reposts/tracks/{$request->track_urn}");
             dd($request , $response );
 
             if ($response->successful()) {
