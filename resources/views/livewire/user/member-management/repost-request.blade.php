f3a179b3d9ec3cc8696a9e012f7ca3ca8c57f166
Md Shariful Islam <devshariful99@gmail.com>
2025-08-08 15:03:36 +0600

    Update repost-request.blade.php

--- resources/views/livewire/user/member-management/repost-request.blade.php
+++ resources/views/livewire/user/member-management/repost-request.blade.php
@@ -164,7 +164,7 @@
                                 <div x-data="{ open: false }" class="inline-block text-left">
                                     <div @click="open = !open" class="flex items-center gap-1 cursor-pointer">
                                         <span
-                                            class="text-slate-700 dark:text-gray-300 font-medium">{{ $repostRequest->targetUser->name }}</span>
+                                            class="text-slate-700 dark:text-gray-300 font-medium">{{ $repostRequest->requester->name }}</span>
                                         <svg class="w-4 h-4 text-gray-600 dark:text-gray-400" fill="none"
                                             viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
