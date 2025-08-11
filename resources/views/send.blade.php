b2b5d19e463d5355e603907597a5f77cf6176837
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-08-11 15:37:19 +0600

    Update send.blade.php

--- resources/views/send.blade.php
+++ resources/views/send.blade.php
@@ -35,7 +35,7 @@
             <div class="flex items-center justify-between">
                 <button type="submit"
                     class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full">
-                    Send Private Message
+                    Send Message
                 </button>
             </div>
         </form>
