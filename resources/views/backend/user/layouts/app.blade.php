ca4d812759a9770f0e88cbfc0133b204188f029e
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-08-14 16:54:44 +0600

    change

--- resources/views/backend/user/layouts/app.blade.php
+++ resources/views/backend/user/layouts/app.blade.php
@@ -5,7 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Sound Cloud') }}</title>
+    <link rel="shortcut icon" href="{{ asset('default_img/logo.svg') }}" type="image/x-icon">
+    <title>
+        {{ isset($title) ? $title . ' - ' : '' }}
+        {{ config('app.name', 'RepostChain') }}
+    </title>
 
     <script>
         (function() {
