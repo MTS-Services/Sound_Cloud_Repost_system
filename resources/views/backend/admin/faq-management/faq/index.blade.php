b0f93b2ac51f35d7a83127acf8ed15af813c64ba
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-08-20 16:11:07 +0600

    update

--- resources/views/backend/admin/faq-management/faq/index.blade.php
+++ resources/views/backend/admin/faq-management/faq/index.blade.php
@@ -1,16 +1,16 @@
+
 <x-admin::layout>
     <x-slot name="title">{{ __('Faq ') }}</x-slot>
     <x-slot name="breadcrumb">{{ __('Faq ') }}</x-slot>
     <x-slot name="page_slug">faq</x-slot>
 
-<section>
+    <section>
 
         <div class="glass-card rounded-2xl p-6 mb-6">
             <div class="flex items-center justify-between">
                 <h2 class="text-xl font-bold text-text-black dark:text-text-white">{{ __('Faq List') }}</h2>
                 <div class="flex items-center gap-2">
-                    <x-button href="{{ route('fm.faq.trash') }}" icon="trash-2" type='secondary'
-                        permission="Faq-trash">
+                    <x-button href="{{ route('fm.faq.trash') }}" icon="trash-2" type='secondary' permission="Faq-trash">
                         {{ __('Trash') }}
                     </x-button>
                     <x-button href="{{ route('fm.faq.create') }}" icon="plus" permission="Faq-create">
@@ -23,11 +23,12 @@
             <table class="table datatable table-zebra">
                 <thead>
                     <tr>
-                        <th>{{ __('SL') }}</th>
-                        <th>{{__('Faq Category')}}</th>
+                        <th width="5%">{{ __('SL') }}</th>
+                        <th>{{ __('Faq Category') }}</th>
                         <th>{{ __('Question') }}</th>
                         <th>{{ __('Status') }}</th>
-                        <th>{{ __('Created Date') }}</th>   
+                        <th>{{ __('Created by') }}</th>
+                        <th>{{ __('Created at') }}</th>
                         <th>{{ __('Action') }}</th>
                     </tr>
                 </thead>
@@ -46,11 +47,10 @@
         <script>
             document.addEventListener('DOMContentLoaded', () => {
                 let table_columns = [
-                    //name and data, orderable, searchable
-                    
                     ['faq_category_id', true, true],
                     ['question', true, true],
-                    ['status', true, true],                 
+                    ['status', true, true],
+                    ['created_by', true, true],
                     ['created_at', true, true],
                     ['action', false, false],
                 ];
@@ -60,7 +60,7 @@
                     displayLength: 10,
                     main_route: "{{ route('fm.faq.index') }}",
                     order_route: "{{ route('update.sort.order') }}",
-                    export_columns: [0, 1, 2, 3,],
+                    export_columns: [0, 1, 2, 3, 4],
                     model: 'Faq',
                 };
                 // initializeDataTable(details);
@@ -78,14 +78,12 @@
                     const route = "{{ route('fm.faq.show', ':id') }}";
 
                     const details = [
-                        
-                        {
-                            label: '{{ __('Faq Category') }}',
-                            key: 'faq_category_id',
-                        },
-                        {
 
-                       
+                        // {
+                        //     label: '{{ __('Faq Category') }}',
+                        //     key: 'faq_category_id.name',
+                        // },
+                        {
                             label: '{{ __('Question') }}',
                             key: 'question',
                         },
@@ -93,22 +91,16 @@
                             label: '{{ __('Description') }}',
                             key: 'description',
                         },
-                       
-                        {
-                            label: '{{ __('Status') }}',
-                            key: 'status',
+                        // {
+                        //     label: '{{ __('Status') }}',
+                        //     key: 'status',
+                        // },
 
-                           
-                        },
-                       
-                        
                     ];
 
-                    showDetailsModal(route, id, '{{ __('Campaign Details') }}', details);
+                    showDetailsModal(route, id, '{{ __('Faq Details') }}', details);
                 });
             });
         </script>
     @endpush
-    
-
 </x-admin::layout>
