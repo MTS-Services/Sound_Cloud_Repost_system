ed4fa01f878d526bcac1e557367e21be64aeeba8
Md Shariful Islam <devshariful99@gmail.com>
2025-09-05 16:48:57 +0600

    Updated

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
-use App\Jobs\TestJob;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class HomeController extends Controller
 {
-    public function landing(): View
+    public function landing(): View|RedirectResponse
     {
+        if (Auth()->guard('web')->check()) {
+            return redirect()->route('user.dashboard');
+        }
         return view('frontend.pages.landing-page.landing');
     }
 }
--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire;
 
+use Auth;
 use Livewire\Component;
 
 class Home extends Component
 {
     public function render()
     {
+        if (Auth::guard('web')->check()) {
+            $this->redirect(route('user.dashboard'), true);
+        }
         return view('frontend.pages.landing-page.landing');
     }
 }
