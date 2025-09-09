915ffc80c8f8fbb8330083c212b812b0c2c490c9
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-09-09 18:01:47 +0600

    updated

--- resources/views/backend/admin/layouts/app.blade.php
+++ resources/views/backend/admin/layouts/app.blade.php
@@ -5,25 +5,26 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    @if (app_setting('favicon') && app_setting('favicon_dark'))
-        <link id="favicon" rel="icon" href="{{ storage_url(app_setting('favicon')) }}" type="image/x-icon">
-    @else
-        <link rel="icon" href="{{ asset('assets/favicons/fav icon 1.svg') }}" type="image/x-icon">
-    @endif
+
+    <link id="favicon" rel="icon" href="{{ storage_url(app_setting('favicon')) }}" type="image/x-icon">
+
     <script>
         function setFavicon(dark = false) {
             let favicon = document.getElementById('favicon');
             favicon.href = dark ?
-                "{{ storage_url(app_setting('favicon_dark')) }}" :
-                "{{ storage_url(app_setting('favicon')) }}";
+                "{{ app_setting('favicon_dark') ? storage_url(app_setting('favicon_dark')) : asset('assets/favicons/fav icon 2 (1).svg') }}" :
+                "{{ app_setting('favicon') ? storage_url(app_setting('favicon')) : asset('assets/favicons/fav icon 1.svg') }}";
         }
+        
         if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
             setFavicon(true);
         }
+        
         window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
             setFavicon(e.matches);
         });
     </script>
+
     <title>
         {{ isset($title) ? $title . ' - ' : '' }}
         {{ config('app.name', 'RepostChain') }}
