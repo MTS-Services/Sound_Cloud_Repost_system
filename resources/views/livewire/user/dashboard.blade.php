bf3e3d26644253f935b1a4c6cd399b744585bb85
Md Shariful Islam <devshariful99@gmail.com>
2025-09-10 12:59:37 +0600

    Update dashboard.blade.php

--- resources/views/livewire/user/dashboard.blade.php
+++ resources/views/livewire/user/dashboard.blade.php
@@ -284,7 +284,7 @@
                 </div>
                 <div class="space-y-2">
                     <p class="text-2xl font-bold text-slate-700 dark:text-white">
-                        {{ $userFollowerAnalysis['averageCredibilityScore'] }}</p>
+                        {{ $userFollowerAnalysis['averageCredibilityScore'] }}%</p>
                     <p class="text-sm flex items-center space-x-1 text-green-400"><span>+3% from last week</span></p>
                 </div>
             </div>
