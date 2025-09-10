093baa5900971256f3e51f74fcd11e71874c791e
Md Shariful Islam <devshariful99@gmail.com>
2025-09-10 12:43:42 +0600

    Update Dashboard.php

--- app/Livewire/User/Dashboard.php
+++ app/Livewire/User/Dashboard.php
@@ -143,6 +143,7 @@ class Dashboard extends Component
 
     public function mount()
     {
+        dd($this->soundCloudService->getAuthUserFollowers());
         $this->userFollowerAnalysis = $this->followerAnalyzer->getQuickStats($this->soundCloudService->getAuthUserFollowers());
         $this->loadDashboardData();
         $this->calculateFollowersLimit();
