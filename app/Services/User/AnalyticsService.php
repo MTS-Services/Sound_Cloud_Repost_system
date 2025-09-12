473a9910ccf777ed65d1c15965e1cac4d0ed8eb0
Md Shariful Islam <devshariful99@gmail.com>
2025-09-12 12:41:03 +0600

    Update AnalyticsService.php

--- app/Services/User/AnalyticsService.php
+++ app/Services/User/AnalyticsService.php
@@ -283,8 +283,8 @@ class AnalyticsService
             ')
             ->groupBy('track_urn')
             // ->orderByDesc('total_views')
-            ->orderBy('total_streams')
-            ->groupBy('total_reposts')
+            ->orderByDesc('total_streams')
+            ->groupByDesc('total_reposts')
             ->get();
 
         // Create paginator
