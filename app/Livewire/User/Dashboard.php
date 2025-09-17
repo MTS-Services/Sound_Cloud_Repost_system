bf030e6c0219be5a68865ba302dea88fc70d5c83
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-09-17 15:26:38 +0600

    Update Dashboard.php

--- app/Livewire/User/Dashboard.php
+++ app/Livewire/User/Dashboard.php
@@ -937,8 +937,9 @@ class Dashboard extends Component
                 }
             }
         }
+        dd($this->searchQuery);
 
-        $response = Http::withToken(user()->token)->get("https://api.soundcloud.com/resolve'", ['url' => $this->searchQuery]);
+        $response = Http::withToken(user()->token)->get("https://api.soundcloud.com/resolve?url='" . $this->searchQuery);
         dd($response->status(), $response->json());
 
 
