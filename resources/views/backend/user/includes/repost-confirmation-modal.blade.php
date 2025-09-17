9e7b3b2aa3b523ac2af3f75273066e96615f6022
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-09-17 14:33:44 +0600

    Update repost-confirmation-modal.blade.php

--- resources/views/backend/user/includes/repost-confirmation-modal.blade.php
+++ resources/views/backend/user/includes/repost-confirmation-modal.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{ showRepostConfirmationModal: @entangle('showRepostConfirmationModal').live }" x-show="(typeof showRepostConfirmationModal !== 'undefined' && showRepostConfirmationModal)"
+<div x-data="{ showRepostConfirmationModal: @entangle('showRepostConfirmationModal').live }" x-show="showRepostConfirmationModal"
     x-cloak x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0 scale-95"
     x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition ease-in duration-200"
     x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95" wire:ignore
