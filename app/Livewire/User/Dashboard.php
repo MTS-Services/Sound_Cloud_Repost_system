24ab6ceebf9862b743880f2d0214655a1b2682a6
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-09-18 15:45:56 +0600

    Track and search query updated

--- app/Livewire/User/Dashboard.php
+++ app/Livewire/User/Dashboard.php
@@ -885,11 +885,6 @@ class Dashboard extends Component
 
     protected function resolveSoundcloudUrl()
     {
-        $this->processSearchData();
-        if ($this->tracks->count() > 0) {
-            return;
-        }
-
         if ($this->playListTrackShow == true && $this->activeTab === 'tracks') {
             $tracksFromDb = Playlist::findOrFail($this->selectedPlaylistId)->tracks()
                 ->where('permalink_url', $this->searchQuery)
@@ -926,21 +921,25 @@ class Dashboard extends Component
                 }
             }
         }
-        
-        $response = Http::withToken(user()->token)->get("https://api.soundcloud.com/resolve?url=" . $this->searchQuery);
+        if (proUser()) {
+            $response = Http::withToken(user()->token)->get("https://api.soundcloud.com/resolve?url=" . $this->searchQuery);
+        } else {
+            $this->dispatch('alert', type: 'error', message: 'Please upgrade to a Pro User to use this feature.');
+        }
 
 
         if ($response->successful()) {
             if ($this->activeTab === 'playlists') {
-                $resolvedTracks = $response->json();
-                if (isset($resolvedTracks['tracks']) && count($resolvedTracks['tracks']) > 0) {
-                    $resolvedPlaylistTracks['tracks'] = $resolvedTracks['tracks'];
-                    $playlistUrn = $resolvedTracks['urn'];
-                    $this->soundCloudService->syncSelfTracks($resolvedPlaylistTracks, $playlistUrn);
+                $resolvedPlaylists = $response->json();
+                if (isset($resolvedPlaylists['tracks']) && count($resolvedPlaylists['tracks']) > 0) {
+                    $this->soundCloudService->unknownPlaylistAdd($resolvedPlaylists);
+                    Log::info('Resolved SoundCloud URL: ' . "Successfully resolved SoundCloud URL: " . $this->searchQuery);
+                } else {
+                    $this->dispatch('alert', type: 'error', message: 'Could not resolve the SoundCloud link. Please check the URL.');
                 }
             } else {
-                $resolvedTrack['tracks'] = $response->json();
-                $this->soundCloudService->syncSelfTracks($resolvedTrack);
+                $resolvedTrack = $response->json();
+                $this->soundCloudService->unknownTrackAdd($resolvedTrack);
             }
             $this->processSearchData();
             Log::info('Resolved SoundCloud URL: ' . "Successfully resolved SoundCloud URL: " . $this->searchQuery);
@@ -965,8 +964,9 @@ class Dashboard extends Component
     {
         if ($this->playListTrackShow == true && $this->activeTab === 'tracks') {
             $tracksFromDb = Playlist::findOrFail($this->selectedPlaylistId)->tracks()
-                ->where('permalink_url', $this->searchQuery)
+                ->where('permalink_url', 'like', '%' . $this->searchQuery . '%')
                 ->get();
+
             if ($tracksFromDb->isNotEmpty()) {
                 $this->allPlaylistTracks = $tracksFromDb;
                 $this->tracks = $this->allPlaylistTracks->take($this->playlistTrackLimit);
@@ -975,8 +975,9 @@ class Dashboard extends Component
             }
         } else {
             if ($this->activeTab == 'tracks') {
-                $tracksFromDb = Track::where('permalink_url', $this->searchQuery)
+                $tracksFromDb = Track::where('permalink_url', 'like', '%' . $this->searchQuery . '%')
                     ->get();
+
                 if ($tracksFromDb->isNotEmpty()) {
                     $this->activeTab = 'tracks';
                     $this->allTracks = $tracksFromDb;
@@ -987,7 +988,7 @@ class Dashboard extends Component
             }
 
             if ($this->activeTab == 'playlists') {
-                $playlistsFromDb = Playlist::where('permalink_url', $this->searchQuery)
+                $playlistsFromDb = Playlist::where('permalink_url', 'like', '%' . $this->searchQuery . '%')
                     ->get();
 
                 if ($playlistsFromDb->isNotEmpty()) {
