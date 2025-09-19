c57e534ea15e545d8e039abe25f27b701240d764
Md Ahoshan Habib <mdahoshanhabib2003@gmail.com>
2025-09-19 09:04:46 +0600

    Final update

--- resources/views/livewire/user/campaign-management/campaign.blade.php
+++ resources/views/livewire/user/campaign-management/campaign.blade.php
@@ -495,21 +495,23 @@
                                         {{ __('Add one to get started with campaigns.') }}
                                     </p>
                                 </div>
-                            @endforelse
-                            <div wire:loading wire:target="searchSoundcloud"
-                                class="text-center py-16 text-orange-600">
-                                <div
-                                    class="w-16 h-16 bg-orange-100 dark:bg-orange-900/20 rounded-full flex items-center justify-center mx-auto mb-4 animate-spin">
-                                    <svg class="w-8 h-8 text-orange-500" xmlns="http://www.w3.org/2000/svg"
-                                        fill="none" viewBox="0 0 24 24">
-                                        <circle class="opacity-25" cx="12" cy="12" r="10"
-                                            stroke="currentColor" stroke-width="4"></circle>
-                                        <path class="opacity-75" fill="currentColor"
-                                            d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 100 16v-4l-3 3 3 3v-4a8 8 0 01-8-8z" />
-                                    </svg>
+                                <div wire:loading wire:target="searchSoundcloud"
+                                    class="flex justify-center items-center">
+                                    <div class="text-center py-16 text-orange-600">
+                                        <div
+                                            class="w-16 h-16 bg-orange-100 dark:bg-orange-900/20 rounded-full flex items-center justify-center mx-auto mb-4 animate-spin">
+                                            <svg class="w-8 h-8 text-orange-500" xmlns="http://www.w3.org/2000/svg"
+                                                fill="none" viewBox="0 0 24 24">
+                                                <circle class="opacity-25" cx="12" cy="12" r="10"
+                                                    stroke="currentColor" stroke-width="4"></circle>
+                                                <path class="opacity-75" fill="currentColor"
+                                                    d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 100 16v-4l-3 3 3 3v-4a8 8 0 01-8-8z" />
+                                            </svg>
+                                        </div>
+                                        <p class="text-sm font-medium">Searching Track...</p>
+                                    </div>
                                 </div>
-                                <p class="text-sm font-medium">Searching Track...</p>
-                            </div>
+                            @endforelse
 
                             {{-- Load More Button for Tracks --}}
                             @if ($hasMoreTracks)
@@ -564,21 +566,23 @@
                                         {{ __('Add one to get started with campaigns.') }}
                                     </p>
                                 </div>
-                            @endforelse
-                            <div wire:loading wire:target="searchSoundcloud"
-                                class="text-center py-16 text-orange-600">
-                                <div
-                                    class="w-16 h-16 bg-orange-100 dark:bg-orange-900/20 rounded-full flex items-center justify-center mx-auto mb-4 animate-spin">
-                                    <svg class="w-8 h-8 text-orange-500" xmlns="http://www.w3.org/2000/svg"
-                                        fill="none" viewBox="0 0 24 24">
-                                        <circle class="opacity-25" cx="12" cy="12" r="10"
-                                            stroke="currentColor" stroke-width="4"></circle>
-                                        <path class="opacity-75" fill="currentColor"
-                                            d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 100 16v-4l-3 3 3 3v-4a8 8 0 01-8-8z" />
-                                    </svg>
+                                <div wire:loading wire:target="searchSoundcloud"
+                                    class="flex justify-center items-center">
+                                    <div class="text-center py-16 text-orange-600">
+                                        <div
+                                            class="w-16 h-16 bg-orange-100 dark:bg-orange-900/20 rounded-full flex items-center justify-center mx-auto mb-4 animate-spin">
+                                            <svg class="w-8 h-8 text-orange-500" xmlns="http://www.w3.org/2000/svg"
+                                                fill="none" viewBox="0 0 24 24">
+                                                <circle class="opacity-25" cx="12" cy="12" r="10"
+                                                    stroke="currentColor" stroke-width="4"></circle>
+                                                <path class="opacity-75" fill="currentColor"
+                                                    d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 100 16v-4l-3 3 3 3v-4a8 8 0 01-8-8z" />
+                                            </svg>
+                                        </div>
+                                        <p class="text-sm font-medium">Searching Playlist...</p>
+                                    </div>
                                 </div>
-                                <p class="text-sm font-medium">Searching playlist...</p>
-                            </div>
+                            @endforelse
 
                             {{-- Load More Button for Playlists --}}
                             @if ($hasMorePlaylists)
