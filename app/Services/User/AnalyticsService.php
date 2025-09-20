f06326c5559c3d90f48f78b08d8f477c19c7cff9
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-09-20 14:59:36 +0600

    Update AnalyticsService.php

--- app/Services/User/AnalyticsService.php
+++ app/Services/User/AnalyticsService.php
@@ -319,7 +319,7 @@ class AnalyticsService
             $currentMetricsByTrack = $this->calculateMetricsByTrack($currentData);
             $previousMetricsByTrack = $this->calculateMetricsByTrack($previousData);
             $trackAnalytics = $this->buildComparisonResultByTrack($currentMetricsByTrack, $previousMetricsByTrack);
-            dd($currentMetricsByTrack, $previousMetricsByTrack, $trackAnalytics);
+            dd($currentMetricsByTrack, $previousMetricsByTrack, $trackAnalytics, $currentData, $previousData);
         }
 
         dd($trackAnalytics);
