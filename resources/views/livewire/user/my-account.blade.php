44bad6f96bd8760a4a492679cd7a3dfb4df40260
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-09-23 18:23:21 +0600

    updated

--- resources/views/livewire/user/my-account.blade.php
+++ resources/views/livewire/user/my-account.blade.php
@@ -743,7 +743,7 @@
                                                         <div class="flex flex-col items-start gap-2">
                                                             {{-- Avatar --}}
                                                             <div class="flex items-center gap-2">
-                                                                <img src="{{ soundcloud_image($repost->source->artwork_url) }}"
+                                                                <img src="{{ soundcloud_image($repost?->source?->artwork_url) }}"
                                                                     class="w-12 h-12 rounded-full object-cover"
                                                                     alt="Track artwork" />
                                                                 <div x-data="{ open: false }"
