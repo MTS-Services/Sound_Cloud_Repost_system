b4ee44cf1b72b34395302c52ad7ce013ef222c2c
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-09-23 16:56:18 +0600

    Update Member.php

--- app/Livewire/User/Member.php
+++ app/Livewire/User/Member.php
@@ -327,7 +327,7 @@ class Member extends Component
             $this->showModal = false;
             return;
         }
-        if (!requestReceiveable($this->user->urn)) {
+        if (requestReceiveable($this->user->urn)) {
             $this->showModal = true;
             $this->activeTab = 'tracks';
 
