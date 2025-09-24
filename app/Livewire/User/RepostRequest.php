63ccad740724d39927a44ad80229d3b6d0c759c9
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-09-24 10:47:39 +0600

    Update RepostRequest.php

--- app/Livewire/User/RepostRequest.php
+++ app/Livewire/User/RepostRequest.php
@@ -225,18 +225,18 @@ class RepostRequest extends Component
 
         if ($canRepost && !$this->playCount) {
             $request = $this->repostRequests->find($requestId);
-           Log::info('requestId: ' . $requestId);
+            Log::info('Direct Request' . json_encode($request));
             // Record analytics for the play
-            $response = $this->analyticsService->recordAnalytics(
-                source: $request->music,
-                actionable: $request,
-                type: UserAnalytics::TYPE_PLAY,
-                genre: $request->music->genre ?? 'anyGenre'
-            );
+            // $response = $this->analyticsService->recordAnalytics(
+            //     source: $request->music,
+            //     actionable: $request,
+            //     type: UserAnalytics::TYPE_PLAY,
+            //     genre: $request->music->genre ?? 'anyGenre'
+            // );
 
-            if ($response != false || $response != null) {
-                $request->increment('playback_count');
-            }
+            // if ($response != false || $response != null) {
+            //     $request->increment('playback_count');
+            // }
 
             $this->playCount = true;
         }
