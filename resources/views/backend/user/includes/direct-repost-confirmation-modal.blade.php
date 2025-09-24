ca105179d1ec10f657def06a80a9baa3ef83029c
Md Shariful Islam <devshariful99@gmail.com>
2025-09-24 15:07:44 +0600

    Update direct-repost-confirmation-modal.blade.php

--- resources/views/backend/user/includes/direct-repost-confirmation-modal.blade.php
+++ resources/views/backend/user/includes/direct-repost-confirmation-modal.blade.php
@@ -40,7 +40,7 @@
                         Credits</span>
                 </div>
                 <div class="flex items-center space-x-3 p-2 border border-gray-200 dark:border-gray-600 rounded-md">
-                    <img src="{{ soundcloud_image($request->music?->artwork_url) }}" alt="Track Cover"
+                    <img src="{{ auth_storage_url($request->music?->user?->avatar) }}" alt="Track Cover"
                         class="w-12 h-12 rounded-md object-cover">
                     <div>
                         <p class="text-sm font-medium text-gray-900 dark:text-gray-100">
