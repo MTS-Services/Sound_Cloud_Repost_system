637f46e5d758a05ab2d60f171778a5f05f93882b
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-10-06 12:47:26 +0600

    Update Dashboard.php

--- app/Livewire/User/Dashboard.php
+++ app/Livewire/User/Dashboard.php
@@ -1177,7 +1177,6 @@ class Dashboard extends Component
         ]);
         $userUrn = $this->request->requester?->urn;
         $checkResponse = $httpClient->get("{$this->soundcloudApiUrl}/me/followings/{$userUrn}");
-        dd($checkResponse->getStatusCode());
 
         if ($checkResponse->getStatusCode() === 200) {
             $this->followed = false;
