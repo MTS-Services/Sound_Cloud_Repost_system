cbc9bea0b1da10096aaa74f221d351bdcc3e0a20
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-10-09 16:20:13 +0600

    Update MyCampaign.php

--- app/Livewire/User/CampaignManagement/MyCampaign.php
+++ app/Livewire/User/CampaignManagement/MyCampaign.php
@@ -1023,7 +1023,7 @@ class MyCampaign extends Component
                 $this->dispatch('alert', type: 'error', message: 'Campaign not found.');
                 return;
             }
-            Log::info('Found Campaign for stopped. id: ', $campaign->id);
+            Log::info('Found Campaign for stopped. id: '. $campaign->id);
             DB::transaction(function () use ($campaign) {
                 $campaign->status = Campaign::STATUS_STOP;
                 $campaign->save();
@@ -1066,7 +1066,7 @@ class MyCampaign extends Component
             });
  
         } catch (\Exception $e) {
-            Log::error('failed to stop campaign. error: ', $e->getMessage());
+            Log::error('failed to stop campaign. error: '. $e->getMessage());
             $this->handleError('Failed to stop campaign', $e, ['campaign_id' => $id]);
             return;
         }
