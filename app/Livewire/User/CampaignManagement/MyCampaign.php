ff2e2b268d23c96e833f26976ffaa4005ac3627d
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-10-09 12:31:44 +0600

    Update MyCampaign.php

--- app/Livewire/User/CampaignManagement/MyCampaign.php
+++ app/Livewire/User/CampaignManagement/MyCampaign.php
@@ -499,7 +499,7 @@ class MyCampaign extends Component
                     'budget_credits' => $this->credit,
                     'user_urn' => user()->urn,
                     'status' => Campaign::STATUS_OPEN,
-                    'max_followers' => $this->maxFollowerEnabled ? $this->maxFollower : 0,
+                    'max_followers' => $this->maxFollowerEnabled ? $this->maxFollower : null,
                     'creater_id' => user()->id,
                     'creater_type' => get_class(user()),
                     'commentable' => $commentable,
