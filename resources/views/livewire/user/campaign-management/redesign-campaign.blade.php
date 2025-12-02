b3b428b46540694e01ccca7ef256a46c083a17e1
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-12-02 16:24:48 +0600

    Update redesign-campaign.blade.php

--- resources/views/livewire/user/campaign-management/redesign-campaign.blade.php
+++ resources/views/livewire/user/campaign-management/redesign-campaign.blade.php
@@ -408,6 +408,7 @@
                                                     <template x-if="!isReposted(campaignId)">
                                                         <div class="flex items-center gap-2 text-white!">
                                                             <svg width="26" height="18" viewBox="0 0 26 18"
+                                                                :class="{ 'stroke-white': isEligibleForRepost(campaignId) }"
                                                                 fill="none" xmlns="http://www.w3.org/2000/svg">
                                                                 <rect x="1" y="1" width="24" height="16"
                                                                     rx="3" fill="none"
@@ -425,6 +426,7 @@
                                                     <template x-if="isReposted(campaignId)">
                                                         <div class="flex items-center gap-2 text-white!">
                                                             <svg class="w-5 h-5" fill="currentColor"
+                                                                class="stroke-white"
                                                                 viewBox="0 0 20 20">
                                                                 <path fill-rule="evenodd"
                                                                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
