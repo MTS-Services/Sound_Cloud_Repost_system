0af336c182b9a4e64f1bf5f053c6321fc129c0d3
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-12-02 16:22:11 +0600

    Update redesign-campaign.blade.php

--- resources/views/livewire/user/campaign-management/redesign-campaign.blade.php
+++ resources/views/livewire/user/campaign-management/redesign-campaign.blade.php
@@ -416,7 +416,9 @@
                                                                     fill="none" stroke="currentColor"
                                                                     stroke-width="2" />
                                                             </svg>
-                                                            <span>{{ user()->repost_price }} Repost</span>
+                                                            <span
+                                                                :class="{ 'text-white': isEligibleForRepost(campaignId) }">{{ user()->repost_price }}
+                                                                Repost</span>
                                                         </div>
                                                     </template>
 
@@ -428,7 +430,7 @@
                                                                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                                                                     clip-rule="evenodd" />
                                                             </svg>
-                                                            <span>Reposted</span>
+                                                            <span class="text-white">Reposted</span>
                                                         </div>
                                                     </template>
                                                 </div>
