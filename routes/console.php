94213222cfc92b30a857c2f9b7e389724385bf87
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-12-14 17:22:14 +0600

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -9,7 +9,7 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('app:repost-request-expire')->everyMinute();
-Schedule::command('app:update-real-followers-daily')->everyMinute();
+Schedule::command('app:update-real-followers-daily')->daily();
 Schedule::command('app:top-performance-source-weekly-update')->weekly();
 Schedule::command('app:ban-untracked-users')->weekly();
 Schedule::command('app:user-plan-expired-inactive')->daily();
